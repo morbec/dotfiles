e28a7d6342c83fd4abdf438c048d28f2474b7db0
Fabio Morbec <morbecfm@gmail.com>
2020-02-01 22:40:52 +0000

    Fix fish plugins

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,21 +1,3 @@
-set PATH /Users/morbec/node_modules/.bin $PATH
-
-set -x LC_ALL en_US.UTF-8
-
-# fnm
-#eval "$(fnm env --multi)"
-
-if [ -r ~/.not-public ]
-  source ~/.not-public
-end
-
 alias config='/usr/local/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias python='python3'
 alias pip='pip3'
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/morbec/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/morbec/Downloads/google-cloud-sdk/path.fish.inc'; end
-set -g fish_user_paths "/usr/local/opt/ruby/bin" $fish_user_paths
-
-# autojump
-# source /usr/local/Cellar/autojump/22.5.3/share/autojump/autojump.fish
--- .config/fish/functions/ld.fish
+++
@@ -1,3 +0,0 @@
-function ld
-  ls -ld .?*
-end
