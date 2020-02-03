65f427e47ddd1a7aebc670d3fdcde76098f1cfd8
Fabio Morbec <morbecfm@gmail.com>
2020-02-03 11:57:53 +0000

    Setup fisher to auto-install packages listes in the fishfile

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,11 @@
 alias config='/usr/local/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias python='python3'
 alias pip='pip3'
+
+# automate the installation process on a new system ...
+#   ... installing packages listed in your fishfile
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
